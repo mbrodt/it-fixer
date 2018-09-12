980d579d95c377750206ac4b971abc0fd40fdf0f
Mads <mkrn@itu.dk>
2018-09-12 22:33:09 +0200

    Removed dead code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,11 +26,6 @@ export class Contact extends React.Component {
   }
 
   handleSubmit = e => {
-    // if (this.state.doCall) {
-    //   this.setState({
-    //     doCall: "Yes"
-    //   })
-    // }
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -110,57 +105,3 @@ export class Contact extends React.Component {
 }
 
 export default Contact
-
-// const Contact = () => {
-//   return (
-//     <section className="text-center py-16 bg-white">
-//       <form
-//         className="w-1/2 shadow-lg mx-auto pb-8"
-//         name="contact"
-//         method="POST"
-//         data-netlify="true"
-//         data-netlify-honeypot="bot-field"
-//       >
-//        <input type="hidden" name="form-name" value="contact" />
-//         <h2 className="text-3xl pt-16 mb-8">Kontakt os</h2>
-//         <input
-//           className="input-field mb-4"
-//           id="name"
-//           name="name"
-//           type="text"
-//           placeholder="Navn"
-//         />
-//         <input
-//           className="input-field mb-4"
-//           id="details"
-//           name="details"
-//           type="text"
-//           placeholder="Email / Tlf"
-//         />
-//         <textarea
-//           className="input-field mb-4"
-//           id="message"
-//           name="message"
-//           type="textarea"
-//           placeholder="Besked"
-//         />
-//         <div className="mb-6">
-//           <label className="block text-grey font-bold">
-//             <input className="mr-2 leading-tight" id="call" type="checkbox" />
-//             <span className="text-xl text-grey-darkest">
-//               Jeg vil gerne ringes op
-//             </span>
-//           </label>
-//         </div>
-//         <button
-//           className="shadow bg-indigo hover:bg-indigo-light focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded text-2xl mb-4"
-//           type="button"
-//         >
-//           SEND
-//         </button>
-//       </form>
-//     </section>
-//   )
-// }
-
-// export default Contact
