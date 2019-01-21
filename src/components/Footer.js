e81a2288e09c0f5002acdbf12cca8031e32e14c1
Mads <mkrn@itu.dk>
2019-01-21 19:07:48 +0100

    add cvr to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <div className="bg-indigo-darker">
     <div className="flex justify-between max-w-xl mx-auto p-4 md:p-8 text-sm">
       <p className="text-white">
-        Copyright &copy; IT-Fixer {new Date().getFullYear()}
+        Copyright &copy; IT-Fixer {new Date().getFullYear()} (CVR: 40141898)
       </p>
       <p className="text-white">
         Lavet af{' '}
